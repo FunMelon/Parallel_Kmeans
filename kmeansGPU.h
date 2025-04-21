14ab4147cbbc6a1b966eed78ce5dcc61a009a38f
FunMelon <Fun_Melon@163.com>
2025-04-21 21:00:36 +0800

    feat: 增加了kmeans并行类（存在bug，无法通过编译）
    - feat:  增加了对vscode配置文件夹的追踪。
    - 增加了kmeansGPU类。
    - 修改了makefile类，使用nvcc取代g++。

---
+++ kmeansGPU.h
@@ -0,0 +1,32 @@
+#ifndef KMEANSGPU_H
+#define KMEANSGPU_H
+
+#include "kmeans.h"
+
+class KmeansGPU : public Kmeans {
+   public:
+    KmeansGPU(int numClusters, int numFeatures, float* clusters, int nsamples);
+    KmeansGPU(int numClusters,
+              int numFeatures,
+              float* clusters,
+              int nsamples,
+              int maxIters,
+              float eplison);
+    virtual void getDistance(const float* d_data);
+    virtual void fit(const float* v_data);
+
+    float* d_clusters;     // [numClusters, numFeatures]，设备端的m_clusters
+    int* d_sampleClasses;  // 设备端的m_sampleClasses
+    float* d_distances;    // 设备端的m_distances
+
+    float* d_minDist;    // [nsamples, ]，设备端的每个样本到各个类的最小距离
+    float* d_loss;       // [nsamples, ]，存储 d_minDist 的规约结果
+    int* d_clusterSize;  //[numClusters,
+                         //]，存储每个类的样本数量，方便求样本均值更新中心点坐标
+
+   private:
+    KmeansGPU(const Kmeans& model);
+    KmeansGPU& operator=(const Kmeans& model);
+};
+
+#endif
--- makefile
+++ makefile
@@ -1,28 +1,36 @@
 # Makefile
+
 # 定义编译器
-CXX = g++
+NVCC = nvcc
 
 # 定义编译选项
 CXXFLAGS = -Wall -std=c++11
+NVCCFLAGS = -arch=sm_52 -std=c++11 -I/home/funmelon/cuda-samples/Common -I/usr/local/cuda/include
+
+# 定义链接选项
+LDFLAGS = -L/usr/local/cuda/lib64 -lcudart
 
 # 定义目标文件
 TARGET = main.out
 
 # 定义源文件和目标文件
-SRC = main.cpp kmeans.cpp
-OBJ = $(SRC:.cpp=.o)
+SRC = main.cpp kmeans.cpp cuda_func.cu kmeansGPU.cpp
+OBJ = main.o kmeans.o kmeansGPU.o cuda_func.o
 
 # 定义默认目标
 all: clean $(TARGET)
 
 # 定义目标文件的构建规则
 $(TARGET): $(OBJ)
-	$(CXX) $(CXXFLAGS) -o $@ $^
+	$(NVCC) $(NVCCFLAGS) $(LDFLAGS) -o $@ $^
 
 # 定义对象文件的构建规则
 %.o: %.cpp
-	$(CXX) $(CXXFLAGS) -c $< -o $@
+	$(NVCC) $(NVCCFLAGS) -c $< -o $@
 
-# 定义清理规则
+%.o: %.cu
+	$(NVCC) $(NVCCFLAGS) -c $< -o $@
+
+# 清理规则
 clean:
 	rm -f $(OBJ) $(TARGET)
